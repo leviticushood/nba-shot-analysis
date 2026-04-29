e9b4c0f9161c2487deeddc97aae3f7e7841a43ea
Levi Hood <hoodlevi2@gmail.com>
2026-04-29 03:50:00 +0000

    created final R file

---
+++ hood_king_523_final.R
@@ -0,0 +1,8 @@
+##------------------------------------------
+## NBA Player Shot Analysis & Modeling
+## INFO 523 - Data Mining and Discovery
+## Levi Hood & Andrew King
+## 30 April 2026
+##------------------------------------------
+
+# This 
